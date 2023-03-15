da2c95ebb1e35f2fe0c5d70b8e906d5b75d559df
Oleksandr Smalko <smalko407@gmail.com>
2023-03-15 18:55:37 +0100

    XLM -> Inversion of Control

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -1,5 +1,19 @@
 package org.example;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        Music music = context.getBean("music", Music.class);
+
+
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
+        context.close();
+    }
 
 }
